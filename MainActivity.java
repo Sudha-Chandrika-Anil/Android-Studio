8a5e40ac0be5e91299035a1e278edd90d1c256b0
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-07-06 23:41:40 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,129 +1,35 @@
-package com.example.calculator_demo;
+package com.example.tab_grid_demo;
 
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
+import android.widget.GridView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public EditText e1, e2;
-    TextView t1;
-    int num1, num2;
-
-    public boolean getNumbers() {
-        // defining the edit text 1 to e1
-        e1 = (EditText) findViewById(R.id.num1);
-
-        // defining the edit text 2 to e2
-        e2 = (EditText) findViewById(R.id.num2);
-
-        // defining the text view to t1
-        t1 = (TextView) findViewById(R.id.result);
-
-        // taking input from text box 1
-        String s1 = e1.getText().toString();
-
-        // taking input from text box 2
-        String s2 = e2.getText().toString();
-
-        if (TextUtils.isEmpty(s1)) {
-            e1.setError("Please enter value 1");
-            return false;
-        }
-        if (TextUtils.isEmpty(s2)) {
-            e2.setError("Please enter value 2");
-            return false;
-        }
-
-        // converting string to int.
-        num1 = Integer.parseInt(s1);
-        // converting string to int.
-        num2 = Integer.parseInt(s2);
-
-        return true;
-    }
-
-    public void doSum(View v) {
-        // get the input numbers
-        if (getNumbers()) {
-            int sum = num1 + num2;
-            t1.setText(Integer.toString(sum));
-        } else {
-            t1.setText("Error Please enter Required Values");
-        }
-    }
-
-    public void clearTextNum1(View v) {
-        e1.getText().clear();
-    }
-
-    public void clearTextNum2(View v) {
-        e2.getText().clear();
-    }
-
-    public void doPow(View v) {
-        // get the input numbers
-        if (getNumbers()) {
-            double result = Math.pow(num1, num2);
-            t1.setText(Double.toString(result));
-        } else {
-            t1.setText("Error Please enter Required Values");
-        }
-    }
-
-    public void doSub(View v) {
-        // get the input numbers
-        if (getNumbers()) {
-            int result = num1 - num2;
-            t1.setText(Integer.toString(result));
-        } else {
-            t1.setText("Error Please enter Required Values");
-        }
-    }
-
-    public void doMul(View v) {
-        // get the input numbers
-        if (getNumbers()) {
-            int result = num1 * num2;
-            t1.setText(Integer.toString(result));
-        } else {
-            t1.setText("Error Please enter Required Values");
-        }
-    }
-
-    public void doDiv(View v) {
-        // get the input numbers
-        if (getNumbers()) {
-            if (num2 == 0) {
-                t1.setText("Error Division by zero");
-            } else {
-                double result = num1 / (num2 * 1.0);
-                t1.setText(Double.toString(result));
-            }
-        } else {
-            t1.setText("Error Please enter Required Values");
-        }
-    }
-
-    public void doMod(View v) {
-        // get the input numbers
-        if (getNumbers()) {
-            double result = num1 % num2;
-            t1.setText(Double.toString(result));
-        } else {
-            t1.setText("Error Please enter Required Values");
-        }
-    }
+    GridView coursesGV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e1 = findViewById(R.id.num1);
-        e2 = findViewById(R.id.num2);
+
+        coursesGV = findViewById(R.id.idGVcourses);
+        ArrayList<CourseModel> courseModelArrayList = new ArrayList<>();
+
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        CourseGVAdapter adapter = new CourseGVAdapter(this, courseModelArrayList);
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
+        coursesGV.setAdapter(adapter);
     }
 }
