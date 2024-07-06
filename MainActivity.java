ad217675b8aabec7916d828ef3f769844f34fe79
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-07-06 23:42:38 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,35 +1,138 @@
-package com.example.tab_grid_demo;
+    package com.example.tablayout_demo;
 
-import android.os.Bundle;
-import android.widget.GridView;
-import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
+    import android.graphics.drawable.Drawable;
+    import android.os.Bundle;
+    import android.text.SpannableString;
+    import android.text.Spanned;
+    import android.text.style.ImageSpan;
 
-public class MainActivity extends AppCompatActivity {
+    import androidx.annotation.NonNull;
+    import androidx.annotation.Nullable;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.content.ContextCompat;
+    import androidx.fragment.app.Fragment;
+    import androidx.fragment.app.FragmentManager;
+    import androidx.fragment.app.FragmentPagerAdapter;
+    import androidx.viewpager.widget.ViewPager;
 
-    GridView coursesGV;
+    import com.google.android.material.tabs.TabLayout;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    import java.util.ArrayList;
 
-        coursesGV = findViewById(R.id.idGVcourses);
-        ArrayList<CourseModel> courseModelArrayList = new ArrayList<>();
+    public class MainActivity extends AppCompatActivity {
 
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        CourseGVAdapter adapter = new CourseGVAdapter(this, courseModelArrayList);
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        courseModelArrayList.add(new CourseModel("Puppy", R.drawable.ic_gfglogo));
-        coursesGV.setAdapter(adapter);
+        // Initialize variables
+        TabLayout tabLayout;
+        ViewPager viewPager;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // assign variable
+            tabLayout=findViewById(R.id.tab_layout);
+            viewPager=findViewById(R.id.view_pager);
+
+            // Initialize array list
+            ArrayList<String> arrayList=new ArrayList<>(0);
+
+            // Add title in array list
+            arrayList.add("Basic");
+            arrayList.add("Advance");
+            arrayList.add("Pro");
+
+            // Setup tab layout
+            tabLayout.setupWithViewPager(viewPager);
+
+            // Prepare view pager
+            prepareViewPager(viewPager,arrayList);
+
+        }
+
+        private void prepareViewPager(ViewPager viewPager, ArrayList<String> arrayList) {
+            // Initialize main adapter
+            MainAdapter adapter=new MainAdapter(getSupportFragmentManager());
+
+            // Initialize main fragment
+            MainFragment mainFragment=new MainFragment();
+
+            // Use for loop
+            for(int i=0;i<arrayList.size();i++)
+            {
+                // Initialize bundle
+                Bundle bundle=new Bundle();
+
+                // Put title
+                bundle.putString("title",arrayList.get(i));
+
+                // set argument
+                mainFragment.setArguments(bundle);
+
+                // Add fragment
+                adapter.addFragment(mainFragment,arrayList.get(i));
+                mainFragment=new MainFragment();
+            }
+            // set adapter
+            viewPager.setAdapter(adapter);
+        }
+
+        private class MainAdapter extends FragmentPagerAdapter {
+            // Initialize arrayList
+            ArrayList<Fragment> fragmentArrayList= new ArrayList<>();
+            ArrayList<String> stringArrayList=new ArrayList<>();
+
+            int[] imageList={R.drawable.puppy,R.drawable.puppy,R.drawable.puppy};
+
+            // Create constructor
+            public void addFragment(Fragment fragment,String s)
+            {
+                // Add fragment
+                fragmentArrayList.add(fragment);
+                // Add title
+                stringArrayList.add(s);
+            }
+
+            public MainAdapter(FragmentManager supportFragmentManager) {
+                super(supportFragmentManager);
+            }
+
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                // return fragment position
+                return fragmentArrayList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                // Return fragment array list size
+                return fragmentArrayList.size();
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+
+                // Initialize drawable
+                Drawable drawable= ContextCompat.getDrawable(getApplicationContext()
+                        ,imageList[position]);
+
+                // set bound
+                drawable.setBounds(0,0,drawable.getIntrinsicWidth(),
+                        drawable.getIntrinsicHeight());
+
+                // Initialize spannable image
+                SpannableString spannableString=new SpannableString(stringArrayList.get(position));
+
+                // Initialize image span
+                ImageSpan imageSpan=new ImageSpan(drawable,ImageSpan.ALIGN_BOTTOM);
+
+                // Set span
+                spannableString.setSpan(imageSpan,0,1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                // return spannable string
+                return spannableString;
+            }
+        }
     }
-}
